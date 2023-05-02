e67fd84a132a0e4b71188356c77c126a2f512a3b
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-05-02 12:25:07 +0000

    crxn 1 for 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -44,7 +44,7 @@ class DBStorage:
         """query on the current database session"""
         new_dict = {}
         for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
+            if cls is None or cls == classes[clss] or cls == clss:
                 objs = self.__session.query(classes[clss]).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
